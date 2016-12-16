95aa24c2c5dece7578e0334b3569788923f63c28
James Reichard <jpqreichard@gmail.com>
2016-12-16 11:57:14 -0500

    add rudimentary non-toggling 'note-fretting'

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-$(document).ready(function(){ 
-  console.log("jq");
+$(document).ready(function(){
   drawGuitarTable(6,22);
-
 })
 
 
 function drawGuitarTable (numStrings, numFrets){
   let table = $("<table><tbody></tbody></table>");
   for (let i = 1; i <= numFrets; i++){
-    let fret = $("<tr></tr>"); 
+    let fret = $("<tr></tr>");
     for (let j = numStrings; j > 0; j--){
       fret.append(`<td class='string${j} fret${i}'></td>`);
     }
     table.append(fret);
-  } 
+  }
   $("main").append(table);
   $("td").append(`<div class="string"></div>`);
   $("td").mouseover(getStringAndFret);
+  $("td").click(addDot)
+}
+
+function addDot(){
+  $(this).children(".string").append("<div class='fretted'></div>")
 }
 
 function getStringAndFret(){
- console.log($(this).attr("class")); 
+  let tuning = {
+    string6: "E",
+    string5: "A",
+    string4: "D",
+    string3: "G",
+    string2: "B",
+    string1: "E"
+  }
+  let gtrStringNumCssClass = $(this).attr("class").split(" ")[0]
+  let guitarStringNote = tuning[gtrStringNumCssClass]
+  let stringNumber = gtrStringNumCssClass.split("string")[1]
+  let fretNum = $(this).attr("class").split("fret")[1]
+ $("#position").text(`${guitarStringNote + stringNumber}: ${fretNum}`)
 }
