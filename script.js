037c266f408f06c1c6979b2c1720a9300e868e9b
James Reichard <jpqreichard@gmail.com>
2017-01-09 13:38:18 -0500

    insert detailed commit message for refactor here

--- script.js
+++ script.js
@@ -5,54 +5,57 @@ $(document).ready(function(){
 
 function drawGuitarTable (numStrings, numFrets){
   let table = $("<table><tbody></tbody></table>");
+  //starts at one for the first fret
   for (let i = 1; i <= numFrets; i++){
     let fret = $("<tr></tr>");
     for (let j = numStrings; j > 0; j--){
-      fret.append(`<td class='string${j} fret${i}'></td>`);
+      let cell = $(`<td data-string='${j}' data-fret='${i}'></td>`)
+      cell.append(`<div data-string='${j}' data-fret='${i}' class="string"></div>`);
+      fret.append(cell);
     }
     table.append(fret);
   }
   $("main").append(table);
-  $("td").append(`<div class="string"></div>`);
-  $("td").mouseover(getStringAndFret);
-  $("td").click( (evt )=> addDot(evt) )
+  // $("td").append(`<div class="string"></div>`);
+  $("td").mouseover( (e) => {
+    let $el = $(e.target)
+    let stringAndFretArr = getStringAndFret($el)
+    writeFrettedNote(...stringAndFretArr)
+  });
+  $("td").click( (e) => {
+    let $el = $(e.target)
+    let stringAndFretArr = getStringAndFret($el)
+    fretNote(...stringAndFretArr)
+  });
 }
 
-function addDot(evt){
-  // evt.stopPropagation()
-  let el = $(evt.target)
-  console.log(el);
-  console.log(!!el.find(".fretted").length);
-
-  if ( el.find(".fretted").length ){
-    el.find(".fretted").remove()
-  } else {
-    if ( el.hasClass(".string") ){
-      el.append("<div class='fretted'></div>")
-    } else {
-      el.children(".string").append("<div class='fretted'></div>")
-    }
-  }
-  console.log(!!el.find(".fretted").length);
-}
 
 function fretNote(string, fret){
-
+  let $foundEl = $("table").find(`div[data-string=${string}][data-fret=${fret}]`)
+  $foundEl.append(
+    "<div data-string=${string} data-fret=${fret} class='fretted'></div>"
+  )
+  $foundEl.click( ()=>{$(this).remove()})
+  console.log(string,fret,$foundEl);
 
 }
 
-function getStringAndFret(){
+function getStringAndFret($el){
+  let stringNum = $el.attr("data-string")
+  let fretNum = $el.attr("data-fret")
+  return [stringNum, fretNum]
+}
+
+function writeFrettedNote(stringNum, fretNum){
   let tuning = {
-    string6: "E",
-    string5: "A",
-    string4: "D",
-    string3: "G",
-    string2: "B",
-    string1: "E"
+    "6": "E",
+    "5": "A",
+    "4": "D",
+    "3": "G",
+    "2": "B",
+    "1": "E"
   }
-  let gtrStringNumCssClass = $(this).attr("class").split(" ")[0]
-  let guitarStringNote = tuning[gtrStringNumCssClass]
-  let stringNumber = gtrStringNumCssClass.split("string")[1]
-  let fretNum = $(this).attr("class").split("fret")[1]
- $("#position").text(`${guitarStringNote + stringNumber}: ${fretNum}`)
+
+  let stringNote = tuning[stringNum]
+  $("#position").text(`${stringNote + stringNum}: ${fretNum}`)
 }
